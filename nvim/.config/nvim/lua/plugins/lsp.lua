2c11c49f84ed3ed4e84b1d250e0a2a250916ee79
albertoodev <amouraammarabderafik@gmail.com>
2026-04-24 10:05:41 +0300

    lsp & simple lualine

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,25 +1,53 @@
-local function on_attach(buf)
-	local opts = { buffer = buf }
-	local map = function(key, fn, desc)
-		vim.keymap.set("n", key, fn, vim.tbl_extend("force", opts, { desc = desc }))
+vim.lsp.config("lua_ls", {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			workspace = {vim.api.nvim_get_runtime_file("", true)},
+			diagnostics = { globals = { "vim","require" } },
+		},
+	},
+})
+
+vim.lsp.config("*", {
+	on_attach = function(_,buf)
+		local opts = { buffer = buf }
+		local map = function(key, fn, desc)
+			vim.keymap.set("n", key, fn, vim.tbl_extend("force", opts, { desc = desc }))
+		end
+		map("gd", vim.lsp.buf.definition, "Go to definition")
+		map("gD", vim.lsp.buf.declaration, "Go to declaration")
+		map("gr", vim.lsp.buf.references, "References")
+		map("gi", vim.lsp.buf.implementation, "Go to implementation")
+		map("K", vim.lsp.buf.hover, "Hover docs")
+		map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+		map("<leader>rn", vim.lsp.buf.rename, "Rename")
+		map("<leader>d", vim.diagnostic.open_float, "Line diagnostics")
+		map("[d", vim.diagnostic.goto_prev, "Prev diagnostic")
+		map("]d", vim.diagnostic.goto_next, "Next diagnostic")
 	end
-	map("gd", vim.lsp.buf.definition, "Go to definition")
-	map("gD", vim.lsp.buf.declaration, "Go to declaration")
-	map("gr", vim.lsp.buf.references, "References")
-	map("gi", vim.lsp.buf.implementation, "Go to implementation")
-	map("K", vim.lsp.buf.hover, "Hover docs")
-	map("<leader>ca", vim.lsp.buf.code_action, "Code action")
-	map("<leader>rn", vim.lsp.buf.rename, "Rename")
-	map("<leader>d", vim.diagnostic.open_float, "Line diagnostics")
-	map("[d", vim.diagnostic.goto_prev, "Prev diagnostic")
-	map("]d", vim.diagnostic.goto_next, "Next diagnostic")
-end
+
+})
+
+vim.lsp.enable("lua_ls")
 
 return {
 	{
 		"williamboman/mason.nvim",
 		build = ":MasonUpdate",
-		opts = {},
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls","stylua"},
+			})
+		end,
 	},
 	{
 		"saghen/blink.cmp",
@@ -36,8 +64,5 @@ return {
 			sources = { default = { "lsp", "path", "snippets", "buffer" } },
 			fuzzy = { implementation = "prefer_rust" },
 		},
-	},
-	_funcs = {
-		on_attach = on_attach,
 	},
 }
