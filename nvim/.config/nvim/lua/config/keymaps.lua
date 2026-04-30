6c0c656aaa14ad1ef69209da1570b2fb6950518f
albertoodev <amouraammarabderafik@gmail.com>
2026-04-30 03:04:40 +0300

    swaync

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -15,6 +15,30 @@ vim.keymap.set("n", "<leader>k", vim.diagnostic.open_float, { desc = "Show diagn
 -- formatting
 vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, { desc = "Format" })
 
+-- ADB WiFi QR code pairing (floating terminal, closes on exit)
+vim.keymap.set("n", "<leader>aa", function()
+	local buf = vim.api.nvim_create_buf(false, true)
+	local width = math.floor(vim.o.columns * 0.5)
+	local height = 20
+	local win = vim.api.nvim_open_win(buf, true, {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = math.floor((vim.o.columns - width) / 2),
+		row = math.floor((vim.o.lines - height) / 2),
+		style = "minimal",
+		border = "rounded",
+		title = " ADB WiFi Pair ",
+		title_pos = "center",
+	})
+	vim.fn.termopen("adb-wifi", {
+		on_exit = function()
+			vim.api.nvim_win_close(win, true)
+		end,
+	})
+	vim.cmd("startinsert")
+end, { desc = "ADB WiFi QR pair" })
+
 -- close float windows with q
 vim.keymap.set("n", "<Esc>", function()
 	vim.cmd.nohlsearch()
